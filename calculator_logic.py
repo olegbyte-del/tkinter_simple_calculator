8136a2ca26073ffc9f376111fc149e8dc47fa635
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-02 20:51:38 +0800

    feat: add calculator operations class
    - implement add, subtract, multiply, divide methods under operation class
    - add zerodivisionerror for error handling in divide method

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,1 +1,18 @@
 # Calculator logic
+
+class operations: 
+    
+    def add(a, b):
+        return a + b 
+    
+    def subtract(a, b): 
+        return a - b 
+    
+    def multiply(a, b):
+        return a * b
+    
+    def divide(a, b): 
+        if b == 0:
+            raise ZeroDivisionError ("Cannot divide by ze")
+        return a / b
+    
