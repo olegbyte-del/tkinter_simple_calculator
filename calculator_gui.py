33823909592bcc73a99e3592faae99bd0002af68
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-02 22:23:40 +0800

    feat: add calculator app icon using PNG
    - load calculator_logo,png using tk,PhotoImage
    - remove object initialization and stored it as an attribute under __init__

--- calculator_gui.py
+++ calculator_gui.py
@@ -3,16 +3,19 @@
 import calculator_logic as cl 
 import tkinter as tk
 
-calculator_actions = cl.CalFeatures()
-
 class CalGui:
     
     def __init__(self, root):
+        
         self.root = root 
         self.root.title("Calculator")
         self.root.geometry("500x500")
-        self.feature = calculator_actions
+        
+        self.feature = cl.CalFeatures()
         self.expression = ""
         
-        self.display = tk.Entry(root, font=("Arial", 20), justify="right")
+        self.display = tk.Entry(root, font=("Arial", 25), justify="left")
         self.display.pack(fill="both", padx=10, pady=10)
+        
+        self.icon = tk.PhotoImage(file="calculator_logo.png")
+        self.root.iconphoto(True, self.icon)
