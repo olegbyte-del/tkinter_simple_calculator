81dd1f6d00ebf6ec1b5e0ac6bef29cccb304ce7d
olegbyte-del <angeloferrer5436@gmail.com>
2026-05-02 21:54:44 +0800

    docs: add docstring and improve calculator history handling
    - Add class and method docstring for CalOperations and CalFeatures
    - Add newline formatting in save_memory for proper history logging

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,30 +1,55 @@
 # Calculator logic
 
 class CalOperations: 
+    """Basic arithmetic operation for the calculator such as addition,
+    subtraction, multiplication, division, and exponent"""
     
+    @staticmethod
     def add(a, b):
+        """Peforms addition"""
         return a + b 
     
+    @staticmethod
     def subtract(a, b): 
+        """Peforms substraction"""
         return a - b 
     
+    @staticmethod
     def multiply(a, b):
+        """Peforms multiplication"""
         return a * b
     
-    def divide(a, b): 
+    @staticmethod
+    def divide(a, b):
+        """Peforms division and handles ZeroDivisionError"""
         if b == 0:
             raise ZeroDivisionError ("Cannot divide by ze")
         return a / b
 
+    @staticmethod
     def expo(a, b):
+        """Peforms power"""
         return a ** b
 
 class CalFeatures:
+    """Handles calculator features such as saving and loading history"""
     
     def __init__(self):
         self.math_expression = ""
         self.result = ""
         
-    def save_memory(self.math_expression, self.result):
+    def save_memory(self, math_expression, result):
+        """Save a calculation to history.txt file"""
+        
         with open("history.txt", "a") as file:
-            file.write(f"{self.math_expression} = {self.result}")   
+            file.write(f"{math_expression} = {result} \n")  
+    
+    def load_memory(self):
+        """Loads all saved calculation history"""
+        
+        try:
+            with open("history.txt", "r") as file:
+                return file.readlines()
+        except FileNotFoundError:
+            return []
+        
